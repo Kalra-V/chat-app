1a7fd81339ae27714c30d7748a2c78d3387720ee
Vansh Kalra <vanshkalra645@gmail.com>
2024-10-16 20:37:17 +0530

    Created Signup, Login and Logout api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express()
 
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
+
+const app = express()
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-    res.send("Server 5001 ready")
-})
+
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//     res.send("Server 5001 ready")
+// })
+
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server Running on port ${PORT}`)
+});
