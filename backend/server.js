e8ae5ee963a45db577a73c0a2b4c5b3dfea91b98
Vansh Kalra <vanshkalra645@gmail.com>
2024-10-27 18:53:13 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -14,6 +15,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve()
 
 
 app.use(express.json());
@@ -23,11 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("Server 5001 ready")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
